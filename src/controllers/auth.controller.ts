64af054bd2775bd6075ac6cbaa4568ff5a008548
Prime Labs <mrcody497@gmail.com>
2026-03-26 23:28:34 +0500

    feat(auth): implement custom exception handling and error middleware

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,20 +1,25 @@
 
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { prisma } from "../lib/prisma.ts";
 import { hashSync,compareSync } from "bcrypt";
 import * as jwt from "jsonwebtoken"
 import { JWT_SECERT } from "../secerts.ts";
+import { BadRequestException } from "../exceptions/badRequest.ts";
+import  { ErrorCode } from "../exceptions/root.ts";
 
-export const signupController = async (req: Request, res: Response) => {
+export const signupController = async (req: Request, res: Response,next:NextFunction) => {
   const { email, password, name } = req.body;
 
   let user = await prisma.user.findFirst({ where: { email } });
-  if (user) return res.status(400).json({ meassage: "user already exists" });
+  if (user) {
+    next( new BadRequestException('User Already Exists',ErrorCode.USER_ALREADY_EXISTS))
+  }
 
   user = await prisma.user.create({
     data: { email, name, password: hashSync(password, 10) },
   });
   res.json(user)
+
 };
 export const loginController = async (req: Request, res: Response) => {
   const { email, password } = req.body;
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { type Express, type Request, type Response } from "express";
 import express from "express";
 import { PORT } from "./secerts.ts";
 import rootRouter from "./routes/index.ts";
+import { errorMiddleware } from "./middlewares/errors.ts";
 const app: Express = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api',rootRouter)
+app.use(errorMiddleware)
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
